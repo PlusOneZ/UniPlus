1b615c8a4fadc07927f57d31498eb1094ad4c385
plusone <zhuozhengyi@icloud.com>
2021-11-21 13:32:24 +0800

    增加了near_place 模块，这个模块是SB框架的

---
+++ near_place/src/test/java/cn/edu/tongji/uniplus/near_place/NearPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.tongji.uniplus.near_place;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearPlaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
