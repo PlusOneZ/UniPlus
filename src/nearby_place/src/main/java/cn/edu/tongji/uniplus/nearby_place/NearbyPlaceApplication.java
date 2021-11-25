9aabfa22be0615596b3f8b1d75c470799da3c756
Yukijudaii <820359253@qq.com>
2021-11-25 22:46:28 +0800

    理了一遍

--- src/nearby_place/src/main/java/cn/edu/tongji/uniplus/nearby_place/NearbyPlaceApplication.java
+++ src/nearby_place/src/main/java/cn/edu/tongji/uniplus/nearby_place/NearbyPlaceApplication.java
@@ -3,13 +3,10 @@ package cn.edu.tongji.uniplus.nearby_place;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.oas.annotations.EnableOpenApi;
-
 @SpringBootApplication
 @EnableOpenApi
 public class NearbyPlaceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(NearbyPlaceApplication.class, args);
     }
-
 }
